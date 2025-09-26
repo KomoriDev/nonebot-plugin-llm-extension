c8f267dcead1e902afbe3a75315431829300ac2e
KomoriDev <mute231010@gmail.com>
2025-09-26 13:35:17 +0800

    :bookmark: v0.1.1

--- nonebot_plugin_llm_extension/__init__.py
+++ nonebot_plugin_llm_extension/__init__.py
@@ -6,7 +6,7 @@ require("nonebot_plugin_alconna")
 from .config import Config
 from .extension import LLMExtension as LLMExtension
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __plugin_meta__ = PluginMetadata(
     name="言令",
     description="利用 LLM 将自然语言转换为机器人指令",
